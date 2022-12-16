28490e3706b8d6a7bb52eff25811652a2e37a1d5
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2022-12-16 10:04:41 -0500

    Update

--- replace-ext.js
+++ replace-ext.js
@@ -1,1 +1,11 @@
+//Remove all content from parent page
+javascript:
+var e = opener.document.getElementsByTagName('html')[0];
+e.removeChild(opener.document.body);
 
+//POC: Add new content on page
+
+//      Element to change:
+//      <h2 id="app-title" class="items-container">Chrome Apps</h2>
+var e = opener.document.getElementById('app-title');
+e.innerHTML = "POC";
